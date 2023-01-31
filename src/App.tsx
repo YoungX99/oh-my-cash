cf0233567c9ab54679714e688e0fb75c520ea1cb
yangjiaxing01 <yangjiaxing01@bilibili.com>
2023-01-31 18:48:39 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import tw from 'twin.macro'
+const Tip = tw.div`font-mono text-3xl`
+
+function App() {
+  return (
+    <div className="App">
+      <Tip>Oh My Cash!</Tip>
+    </div>
+  )
+}
+
+export default App
